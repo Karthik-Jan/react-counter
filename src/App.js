92e2d8b97859c9b1210409259a191e6c0955f4b9
Karthik-Jangapally <karthikj0990@gmail.com>
2020-03-31 14:57:18 -0500

    added all files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            count: 0
+        }
+    }
+
+    handleCounter = flag => {
+        const {count} = this.state;
+
+        if (flag === "add") {
+            this.setState(prevProp => {
+                debugger;
+                return {count: prevProp.count + 1}
+            })
+        } else if (flag === "subtract") {
+            this.setState(prevProp => {
+                return {count: prevProp.count - 1}
+            })
+        } else {
+            this.setState({count: 0})
+        }
+    };
+
+    render() {
+        const {count} = this.state;
+
+        return (
+            <div className="App">
+                <h3>Counter</h3>
+                <div className="container">
+                    <h2>{count}</h2>
+                    <button onClick={() => this.handleCounter("add")}>+</button>
+                    <button onClick={() => this.handleCounter("subtract")}>-</button>
+                    <div>
+                        <button onClick={() => this.handleCounter("reset")}>Reset</button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
